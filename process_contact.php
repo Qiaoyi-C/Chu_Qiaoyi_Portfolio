b5ec6298855fe78f83d3e8f01e3fa7aea83be6d1
Qiaoyi-C <joy502923@gmail.com>
2025-03-06 09:17:30 -0500

    modified contact.php

--- process_contact.php
+++ process_contact.php
@@ -37,12 +37,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if (mail($to, $subject, $email_message, $headers)) {
                 echo "<script>
                     alert('Thank you for your message. We will get back to you shortly!');
-                    window.location.href = 'index.html';
+                    window.location.href = 'index.php';
                 </script>";
             } else {
                 echo "<script>
                     alert('Message sent but email notification failed.');
-                    window.location.href = 'index.html';
+                    window.location.href = 'index.php';
                 </script>";
             }
         } catch (PDOException $e) {
